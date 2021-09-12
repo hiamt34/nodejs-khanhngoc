03894db217e11f0c1d9e9c80d29fdb0c608fe524
hiiamt34 <d17dccn195@gmail.com>
2021-09-12 16:55:53 +0700

    update1 commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+//????
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
